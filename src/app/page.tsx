314d0e37866c0bbe9be7b96f0b1ccebc72a6663a
Pranav <pranavpatidar28@gmail.com>
2025-08-20 11:56:36 +0530

    Basic HeroSection and Navbar implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
-'use client'
+"use client";
+import HeroSection from "@/components/HeroSection";
+import { Navbar } from "@/components/Navbar";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import {ScrollTrigger, SplitText} from "gsap/all";
+import { ScrollTrigger, SplitText } from "gsap/all";
 
-gsap.registerPlugin( ScrollTrigger, SplitText);
-
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function Home() {
   return (
     <div>
-      
+      <Navbar />
+      <HeroSection />
+      <div className="h-screen bg-black"></div>
     </div>
   );
 }
